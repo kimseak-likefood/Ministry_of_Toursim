24f7d11e9178d615622562d65bdc5c4eebb88e3d
Cunnwee <kelwinjr11@gmail.com>
2026-06-04 00:10:26 +0700

    updated pictures

---
+++ storage/framework/views/45271eaeb724e495fbb80aef95da9160.php
@@ -0,0 +1,13 @@
+<li class="nav-item dropdown my-auto">
+    <a href="#" class="nav-link dropdown-toggle p-0 user" id="navbarDropdown3"
+        data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="d-inline-block p-2 theme-bg-primary rounded-circle lh-1">
+            <i class="bi bi-person"></i>
+        </span>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-end sub-menu" aria-labelledby="navbarDropdown3">
+        <li><a class="dropdown-item" href="<?php echo e(route('login')); ?>">Sign in</a></li>
+        <li><a class="dropdown-item" href="<?php echo e(route('register')); ?>">Register</a></li>
+    </ul>
+</li>
+<?php /**PATH C:\Users\USER\Herd\Ministry_of_Toursim\resources\views/partials/nav/user.blade.php ENDPATH**/ ?>
