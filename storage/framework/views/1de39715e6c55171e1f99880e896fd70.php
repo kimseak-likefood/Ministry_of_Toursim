d82c17c5664dfa7996a820f4a6172a17b8b95602
Vnak15-make <skun1@paragoniu.edu.kh>
2026-06-04 15:46:32 +0700

    changed pictures, removed un needed info, changed home page.

--- storage/framework/views/1de39715e6c55171e1f99880e896fd70.php
+++ storage/framework/views/1de39715e6c55171e1f99880e896fd70.php
@@ -11,8 +11,39 @@
     <?php echo $__env->make('sections.recommended', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
     <?php echo $__env->make('sections.special-offers', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
     <?php echo $__env->make('sections.destinations', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+
+   <div class="showcase-banner" style="
+    background: url('/assets/images/section/banner-image.jpg') center center / cover no-repeat;
+    height: 400px;
+    width: 100%;
+    margin-bottom: 15rem;
+    margin-top: 8rem;
+    position: relative;
+">
+    <div style="
+        position: absolute;
+        inset: 0;
+        background: linear-gradient(180deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.55) 100%);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 2rem;
+    ">
+        <h1 style="color: #fff; font-weight: 700; font-size: 2.8rem; text-shadow: 0 2px 10px rgba(0,0,0,0.4); margin-bottom: 0.75rem;">
+            Discover the Kingdom of Wonder
+        </h1>
+        <p style="color: rgba(255,255,255,0.85); font-size: 1.1rem; max-width: 550px; margin-bottom: 1.5rem;">
+            Explore Cambodia's ancient temples, vibrant culture, and breathtaking landscapes
+        </p>
+    </div>
+</div>
+
+
+
     <?php echo $__env->make('sections.experience', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-    <?php echo $__env->make('sections.popular-routes', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+    <?php $__env->startSection('content'); ?>
     <?php echo $__env->make('sections.testimonials', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
     <?php echo $__env->make('sections.blog', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
     <?php echo $__env->make('sections.newsletter', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
--- storage/framework/views/3a819d301995f4aeac06945fe1a0249f.php
+++ storage/framework/views/3a819d301995f4aeac06945fe1a0249f.php
@@ -11,35 +11,15 @@
             <!-- Offer 1 -->
             <div class="col-12 col-md-6 mb-3 mb-md-0 overflow-hidden hoverShine">
                 <div class="box product01">
-                    <div class="content">
-                        <p class="fs-1 theme-heading theme-text-white mb-4">Things to do on <br>your trip</p>
-                        <div class="custom-button">
-                            <a href="javascript:void(0)" class="btn btn-shop small">
-                                Learn More
-                                <i class="bi bi-arrow-up-right fs-6 ms-2"></i>
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
 
             <!-- Offer 2 -->
             <div class="col-12 col-md-6 mb-3 mb-md-0 overflow-hidden hoverShine">
                 <div class="box product02">
-                    <div class="content">
-                        <p class="fs-6 mb-2 theme-text-white">Enjoy Summer Deals</p>
-                        <p class="fs-1 theme-heading theme-text-white mb-4">Up to 70% Discount!</p>
-                        <div class="custom-button">
-                            <a href="javascript:void(0)" class="btn btn-shop small">
-                                View Deal
-                                <i class="bi bi-arrow-up-right fs-6 ms-2"></i>
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </div>
 
         </div>
     </div>
-</section>
-<?php /**PATH C:\Users\dogso\OneDrive\Documents\GitHub\Ministry_of_Toursim\resources\views/sections/special-offers.blade.php ENDPATH**/ ?>
+</section><?php /**PATH C:\Users\dogso\OneDrive\Documents\GitHub\Ministry_of_Toursim\resources\views/sections/special-offers.blade.php ENDPATH**/ ?>
