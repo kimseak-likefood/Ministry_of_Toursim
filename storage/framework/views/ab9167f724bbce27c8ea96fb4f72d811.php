38f4529d1d171b61b94a09ab2cae041ad77cd516
Vnak15-make <skun1@paragoniu.edu.kh>
2026-06-04 12:49:30 +0700

    did header and recommened in home

--- storage/framework/views/ab9167f724bbce27c8ea96fb4f72d811.php
+++ storage/framework/views/ab9167f724bbce27c8ea96fb4f72d811.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg py-3 py-lg-0 px-0">
             <a class="navbar-brand" href="/">
-                <img src="/assets/images/logo.png" alt="Brand Logo" title="Brand Logo" class="img-fluid">
+                <img src="/assets/images/logo.png" alt="Brand Logo" title="Brand Logo" class="logo.png"style="width: 90px; height: auto;">
             </a>
             <button class="navbar-toggler px-1 btn rounded-0" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
