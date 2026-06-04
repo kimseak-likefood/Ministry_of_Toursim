4993f998a5b259bb2d479144175839e943823491
Vnak15-make <skun1@paragoniu.edu.kh>
2026-06-05 01:14:58 +0700

    changed activity blade

--- storage/framework/views/7c656cfd9b7f3ce9e62cf16d1b0e4f73.php
+++ storage/framework/views/7c656cfd9b7f3ce9e62cf16d1b0e4f73.php
@@ -1,11 +1,8 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <h1>page for stats</h1>
-</head>
-<body>
-    
-</body>
-</html><?php /**PATH C:\Users\dogso\OneDrive\Documents\GitHub\Ministry_of_Toursim\resources\views/pages/statistics.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('partials.header', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+
+<?php $__env->startSection('title', 'Statistics'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <h1>Page for stats</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\dogso\OneDrive\Documents\GitHub\Ministry_of_Toursim\resources\views/pages/statistics.blade.php ENDPATH**/ ?>
