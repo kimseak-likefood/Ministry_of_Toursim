34e378e285c401fba1574587c984be5fdb76fb50
Vattanak Samnang <vsamnang@paragoniu.edu.kh>
2026-06-11 21:43:59 +0700

    Made the Tourism What-to-do less empty
    Yep

---
+++ storage/framework/views/d061cf0369687dc11cdef713d95fd5b2.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="ThemesLay">
+    <title><?php echo $__env->yieldContent('title', 'Ministry of Tourism ក្រសួងទេសចរណ៍'); ?></title>
+    <link rel="icon" type="image/png" sizes="80x80" href="/assets/images/MOT1-logo.png">
+    <link href="/assets/css/main.css" rel="stylesheet">
+    <?php echo $__env->yieldPushContent('styles'); ?>
+
+    <style>
+        .profile-picture {
+          width: 100%;
+          height: auto;
+          border-radius: 50%;
+          aspect-ratio: 1/1;
+          object-fit: cover;
+      } 
+
+      .profile-card{
+        padding: 10rem 10rem;
+      }
+    </style>
+</head>
+<body>
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <?php echo $__env->make('partials.footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+    
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <?php echo $__env->yieldPushContent('scripts'); ?>
+</body>
+</html><?php /**PATH C:\Users\Asus\Herd\MinistryOfTourism\resources\views/layouts/app.blade.php ENDPATH**/ ?>
